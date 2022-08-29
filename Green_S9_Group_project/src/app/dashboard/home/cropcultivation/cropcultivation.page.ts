90ece412264aef8336f22d85224ef4a3bdc32be4
Rifkhankhan <you@example.comclscls>
2022-08-30 00:17:30 +0530

    translate cropcultivation page

--- Green_S9_Group_project/src/app/dashboard/home/cropcultivation/cropcultivation.page.ts
+++ Green_S9_Group_project/src/app/dashboard/home/cropcultivation/cropcultivation.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { HomeService } from 'src/app/dashboard/home/HomeServices/home.service';
 import { Crop } from 'src/app/models/crop.model';
+import { PopoverPage } from '../../popover/popover.page';
 
 @Component({
 	selector: 'app-cropcultivation',
@@ -12,7 +15,9 @@ import { Crop } from 'src/app/models/crop.model';
 export class CropcultivationPage implements OnInit, OnDestroy {
 	constructor(
 		private Router: ActivatedRoute,
-		private homeService: HomeService
+		private homeService: HomeService,
+    private popoverCtrl: PopoverController,
+		private transalteService: TranslateService
 	) {}
 	crop: Crop;
 	cropSub: Subscription;
@@ -45,6 +50,14 @@ export class CropcultivationPage implements OnInit, OnDestroy {
 		});
 	}
 
+	async openLanguagePopover(event: Event) {
+		const popover = await this.popoverCtrl.create({
+			component: PopoverPage,
+			event: event
+		});
+
+		await popover.present();
+	}
 	ngOnDestroy(): void {
 		if (this.cropSub || this.idSub) {
 			this.cropSub.unsubscribe();
