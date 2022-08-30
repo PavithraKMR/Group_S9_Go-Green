63190c4863722f7349cbbf70525b7c3dfb92cc6d
Rifkhankhan <you@example.comclscls>
2022-08-30 11:26:54 +0530

    created animalintervention route in backend

---
+++ Server/routes/AnimalIntervention.js
@@ -0,0 +1,28 @@
+const express = require('express');
+var router = express.Router();
+var TipController = require('../Controllers/Tip');
+var diseaseController = require('../Controllers/Disease');
+var fileUpload = require('../Middleware/file-upload');
+const multer = require('multer');
+// const storage = require('../Middleware/file-upload')
+
+router.get('/Interventions/:interventionId', TipController.getTip);
+router.get('/getInterventions/:cropName', TipController.getTipsByCropName);
+
+router.post('/createIntervention', TipController.createTip);
+router.patch(
+	'/cropInterventions/update/:interventionId',
+	TipController.updateTipsByCropName
+);
+router.delete(
+	'/cropInterventions/delete/:interventionId',
+	TipController.deleteTipByCropName
+);
+
+router.post(
+	'/createIntervention',
+	fileUpload.single('image'),
+	diseaseController.createDisease
+);
+
+module.exports = router;
