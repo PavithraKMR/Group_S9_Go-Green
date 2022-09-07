c12888762bacaeae8ba0640b38de1b4c76320c65
Pavithra98611 <pavi19980611@gmail.com>
2022-09-07 10:57:42 +0530

    Fertilizer Calculation

--- Green_S9_Group_project/src/app/dashboard/home/cropcultivation/fertilizer-calculator/fertilizer-calculator.page.ts
+++ Green_S9_Group_project/src/app/dashboard/home/cropcultivation/fertilizer-calculator/fertilizer-calculator.page.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -10,9 +11,27 @@ import { HomeService } from '../../HomeServices/home.service';
   templateUrl: './fertilizer-calculator.page.html',
   styleUrls: ['./fertilizer-calculator.page.scss'],
 })
+
 export class FertilizerCalculatorPage implements OnInit {
 
+  area: number;
+  type: any = null;
+  buttonDisabled: boolean = true;
 
+  nitrogen:any;
+  posporus:any;
+  pottasium:any;
+  zinc:any;
+
+  fert = {
+    'Nitrogen' : null,
+    'Posperous' : null,
+    'Potassium' : null,
+    'Zinc' : null
+  }
+
+  calculated:boolean = false;
+  
   constructor(private homeService: HomeService,private route: ActivatedRoute,private router: Router) { }
 
   tipSub:Subscription
@@ -35,6 +54,7 @@ export class FertilizerCalculatorPage implements OnInit {
         this.crop = crop
       })
     })
+
   }
 
   ionViewWillEnter()
@@ -70,4 +90,144 @@ export class FertilizerCalculatorPage implements OnInit {
     }
   }
 
+
+
+  validate(){
+    
+    if (this.area > 0 && this.type !== null) {
+      this.buttonDisabled = false;
+    } else {
+      this.buttonDisabled = true;
+    }
+  }
+
+  Calculate(){
+    if(this.crop.name === "Onion"){
+      this.OnionFertCalculator();
+    }else if(this.crop.name === "Carrot"){
+      this.CarrotFertCalculator();
+    }else if(this.crop.name === "Ginger"){
+      this.GingerFertCalculator();
+    }else if(this.crop.name === "Pottato"){
+      this.PottatoFertCalculator();
+    }else if(this.crop.name === "Turmeric"){
+      this.TurmericFertCalculator();
+    }else if(this.crop.name === "Paddy"){
+      this.PaddyFertCalculator();
+    }
+
+    
+  }
+
+  OnionFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Nitrogen'] = (4.5 * c_area).toFixed(2);
+    this.fert['Posperous'] = (10 * c_area).toFixed(2);
+    this.fert['Potassium'] = (5 * c_area).toFixed(2);
+
+    this.calculated = true;
+    
+    
+  }
+
+  CarrotFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Nitrogen'] = ((110/0.015) * c_area).toFixed(2);
+    this.fert['Posperous'] = ((85/0.015) * c_area).toFixed(2);
+    
+    this.calculated = true;
+  }
+
+  GingerFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Posperous'] = ((50/7.4322432) * c_area ).toFixed(2);
+
+    this.calculated = true;
+  }
+
+  PottatoFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Nitrogen'] = (16.5 * c_area).toFixed(2);
+    this.fert['Posperous'] = (27.5 * c_area).toFixed(2);
+    this.fert['Potassium'] = (12.5 * c_area).toFixed(2);
+
+    this.calculated = true;
+  }
+
+  TurmericFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Posperous'] = ((52/7.4322432) * c_area ).toFixed(2);
+
+    this.calculated = true;
+  }
+
+  PaddyFertCalculator(){
+    let c_area = 0;
+    if(this.type === "hect"){
+      c_area = this.area*10000;
+      
+    }
+    if(this.type === "acr"){
+      c_area = this.area*4046.86;
+    }else{
+      c_area = this.area;
+    }
+
+    this.fert['Nitrogen'] = (0.5 * c_area).toFixed(2);
+    this.fert['Posperous'] = (3.5 * c_area).toFixed(2);
+    this.fert['Potassium'] = (1.5 * c_area).toFixed(2);
+    this.fert['Zinc'] = (0.2 * c_area).toFixed(2);
+
+    this.calculated = true;
+  }
 }
