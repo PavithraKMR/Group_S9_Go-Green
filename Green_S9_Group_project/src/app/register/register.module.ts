0cf216f6415e6b48818d89c5372c13ee11d545ca
Rifkhankhan <you@example.comclscls>
2022-09-20 11:30:38 +0530

    fixed some errors

--- Green_S9_Group_project/src/app/register/register.module.ts
+++ Green_S9_Group_project/src/app/register/register.module.ts
@@ -1,3 +1,4 @@
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,6 +14,7 @@ import { RegisterPage } from './register.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    Ng2SearchPipeModule,
     RegisterPageRoutingModule
   ],
   declarations: [RegisterPage]
